909763f397a9821feb52775cabd5e5e39a1cc832
chliviu <215432+chliviu@users.noreply.github.com>
2019-10-21 05:09:48 +0300

    update the oauth2 URLs for authorization and access token

--- linkedin_v2/linkedin.py
+++ linkedin_v2/linkedin.py
@@ -81,8 +81,8 @@ class LinkedInAuthentication(object):
     Implements a standard OAuth 2.0 flow that involves redirection for users to
     authorize the application to access account data.
     """
-    AUTHORIZATION_URL = 'https://www.linkedin.com/uas/oauth2/authorization'
-    ACCESS_TOKEN_URL = 'https://www.linkedin.com/uas/oauth2/accessToken'
+    AUTHORIZATION_URL = 'https://www.linkedin.com/oauth/v2/authorization'
+    ACCESS_TOKEN_URL = 'https://www.linkedin.com/oauth/v2/accessToken'
 
     def __init__(self, key, secret, redirect_uri, permissions=None):
         self.key = key
